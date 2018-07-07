7294c00b2eaa378b3a97608e203c0048fa7f4cad
igromanru <igromanru@yahoo.de>
2018-07-07 16:57:11 +0200

    Moved function into namespace. Added FindPatternDump, FindPatternExternal, ReadRIPAddress and ReadRIPAddressPtr

--- FindPattern.h
+++ FindPattern.h
@@ -6,111 +6,134 @@
 #include <psapi.h>
 
 namespace IgroWidgets
-{
-	class FindPattern
+{	
+	size_t FindPatternDump(const unsigned char * dump, const size_t length, const unsigned char* pattern, const char* mask, size_t & outOffset)
 	{
-	public:
-		static uintptr_t ScanExternal(const HANDLE processHanlde, const HMODULE moduleHandle, const unsigned char* pattern, const char* mask)
+		size_t patternPos = 0;
+		const size_t maskLength = std::strlen(mask) - 1;
+
+		size_t tmpOffset = -1;
+		for (size_t offset = 0; offset < length - 1; ++offset)
 		{
-			uintptr_t result = 0;
-			MODULEINFO info = { };
-			const auto moduleAddress = reinterpret_cast<uintptr_t>(moduleHandle);
-			if(GetModuleInformation(processHanlde, moduleHandle, &info, sizeof(MODULEINFO)))
-			{
-				byte* buffer = new byte[info.SizeOfImage];
-				if(ReadProcessMemory(processHanlde, moduleHandle, buffer, info.SizeOfImage, nullptr) != 0)
+			if (dump[offset] == pattern[patternPos] || mask[patternPos] == '?')
+			{					
+				if (mask[patternPos + 1] == '\0')
 				{
-					const auto offset = ScanDump(buffer, info.SizeOfImage, pattern, mask);
-					if(offset > -1)
-					{
-						result = moduleAddress + offset;
-					}
-					delete[] buffer;
-				}				
+					outOffset = (offset - maskLength);
+					return true;
+				}
+				if(tmpOffset == -1)
+				{
+					tmpOffset = offset;
+				}
+				patternPos++;
 			}
-			return result;
+			else
+			{
+				if(tmpOffset > -1)
+				{
+					offset = tmpOffset;
+					tmpOffset = -1;
+				}
+				patternPos = 0;
+			}
 		}
 
-		static uintptr_t Scan(const HANDLE processHanlde, const HMODULE moduleHandle, const unsigned char* pattern, const char* mask)
+		return false;
+	}
+
+	uintptr_t FindPatternExternal(const HANDLE processHanlde, const HMODULE moduleHandle, const unsigned char* pattern, const char* mask)
+	{
+		uintptr_t result;
+		MODULEINFO info = { };
+		const auto moduleAddress = reinterpret_cast<uintptr_t>(moduleHandle);
+		if(GetModuleInformation(processHanlde, moduleHandle, &info, sizeof(MODULEINFO)))
 		{
-			uintptr_t result = 0;
-			MODULEINFO info = { };			
-			if(GetModuleInformation(processHanlde, moduleHandle, &info, sizeof(MODULEINFO)))
+			unsigned char* buffer = new unsigned char[info.SizeOfImage];
+			if(ReadProcessMemory(processHanlde, moduleHandle, buffer, info.SizeOfImage, nullptr) != 0)
 			{
-				result = Scan(reinterpret_cast<uintptr_t>(moduleHandle), info.SizeOfImage, pattern, mask);
+				size_t offset;
+				if(IgroWidgets::FindPatternDump(buffer, info.SizeOfImage, pattern, mask, offset))
+				{
+					result = moduleAddress + offset;
+				}
 			}
-			return result;
+			delete[] buffer;
+		}
+		return result;
+	}
+
+	uintptr_t FindPattern(const uintptr_t start, const size_t length, const unsigned char* pattern, const char* mask)
+	{
+		size_t pos = 0;
+		const auto maskLength = std::strlen(mask) - 1;
+
+		uintptr_t tmpAddress = 0;
+		const auto moduleLength = start + length - 1;
+		for (auto it = start; it < moduleLength; ++it)
+		{
+			if (*reinterpret_cast<unsigned char*>(it) == pattern[pos] || mask[pos] == '?')
+			{					
+				if (mask[pos + 1] == '\0')
+				{
+					return it - maskLength;
+				}
+				if(tmpAddress == 0)
+				{
+					tmpAddress = it;
+				}
+				pos++;
+			}
+			else
+			{
+				if(tmpAddress > 0)
+				{
+					it = tmpAddress;
+					tmpAddress = 0;
+				}
+				pos = 0;
+			}
 		}
 
-		static uintptr_t Scan(const uintptr_t start, const size_t length, const unsigned char* pattern, const char* mask)
+		return 0;
+	}
+
+	uintptr_t FindPattern(const HANDLE processHanlde, const HMODULE moduleHandle, const unsigned char* pattern, const char* mask)
+	{
+		uintptr_t result;
+		MODULEINFO info = { };			
+		if(GetModuleInformation(processHanlde, moduleHandle, &info, sizeof(MODULEINFO)))
 		{
-			size_t pos = 0;
-			const auto maskLength = std::strlen(mask) - 1;
-
-			uintptr_t tmpAddress = 0;
-			const auto moduleLength = start + length - 1;
-			for (auto it = start; it < moduleLength; ++it)
-			{
-				if (*reinterpret_cast<unsigned char*>(it) == pattern[pos] || mask[pos] == '?')
-				{					
-					if (mask[pos + 1] == '\0')
-					{
-						return it - maskLength;
-					}
-					if(tmpAddress == 0)
-					{
-						tmpAddress = it;
-					}
-					pos++;
-				}
-				else
-				{
-					if(tmpAddress > 0)
-					{
-						it = tmpAddress;
-						tmpAddress = 0;
-					}
-					pos = 0;
-				}
-			}
-
-			return 0;
+			result = IgroWidgets::FindPattern(reinterpret_cast<uintptr_t>(moduleHandle), info.SizeOfImage, pattern, mask);
 		}
+		return result;
+	}
 
-		static int64_t ScanDump(const byte * dump, const size_t length, const unsigned char* pattern, const char* mask)
+	uintptr_t ReadRIPAddress(const HANDLE processHanlde, const uintptr_t address, const uint32_t firstOffset, const uint32_t secondOffset)
+	{
+		uintptr_t result;
+		uint32_t offset;
+		if(ReadProcessMemory(processHanlde, (LPCVOID)(address + firstOffset), &offset, sizeof(uint32_t), nullptr) != 0)
 		{
-			size_t patternPos = 0;
-			const auto maskLength = std::strlen(mask) - 1;
+			result = address + offset + secondOffset;
+		}
+		return result;
+	}
 
-			int64_t tmpOffset = -1;
-			for (int64_t offset = 0; offset < length - 1; ++offset)
+	uintptr_t ReadRIPAddressPtr(const HANDLE processHanlde, const uintptr_t address, const uint32_t firstOffset, const uint32_t secondOffset)
+	{
+		uintptr_t result;
+		uint32_t offset;
+		if(ReadProcessMemory(processHanlde, (LPCVOID)(address + firstOffset), &offset, sizeof(uint32_t), nullptr) != 0)
+		{
+			uintptr_t tmpResult;
+			if(ReadProcessMemory(processHanlde, (LPCVOID)(address + offset + secondOffset), &tmpResult, sizeof(uintptr_t), nullptr) != 0)
 			{
-				if (*reinterpret_cast<unsigned char*>(dump[offset]) == pattern[patternPos] || mask[patternPos] == '?')
-				{					
-					if (mask[patternPos + 1] == '\0')
-					{
-						return offset - maskLength;
-					}
-					if(tmpOffset == -1)
-					{
-						offset = tmpOffset;
-					}
-					patternPos++;
-				}
-				else
-				{
-					if(tmpOffset > -1)
-					{
-						offset = tmpOffset;
-						tmpOffset = -1;
-					}
-					patternPos = 0;
-				}
+				result = tmpResult;
 			}
-
-			return -1;
-		}		
-	};	
+		}
+		return result;
+	}
 }
 
 
